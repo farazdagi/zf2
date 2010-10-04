ccd092f941e4216f4a3d7c0ed984574075f179ef
Victor Farazdagi <simple.square@gmail.com>
2010-10-04 17:31:30 +0400

    \Zend\OpenId: First UTs passing

---
+++ library/Zend/OpenId/Storage/AbstractStorage.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_OpenId
+ * @subpackage Zend_OpenId_Storage
+ * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace Zend\OpenId\Storage;
+use Zend\OpenId\Storage;
+
+/**
+ * Base storage implementation
+ *
+ * @category   Zend
+ * @package    Zend_OpenId
+ * @subpackage Zend_OpenId_Storage
+ * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+abstract class AbstractStorage
+    implements Storage
+{
+    /**
+     * Reset the storage to its initial state
+     *
+     * Internally cleanupAssociations(), cleanupNonces(), and 
+     * cleanupDiscoveryInfo() are called
+     *
+     * @return \Zend\OpenId\Storage
+     */
+    public function cleanup()
+    {
+        $this->cleanupAssociations()
+             ->cleanupDiscoveryInfo()
+             ->cleanupNonces();
+        return $this;
+    }
+}
