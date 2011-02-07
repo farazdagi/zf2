013fdaa0b8b0f48c4112896b5336ba6010c4cf2b
Victor Farazdagi <simple.square@gmail.com>
2011-02-07 13:21:32 +0300

    Zend\OpenId: Yadis + XRI XRDS Parsers and Unit Tests
    Yadis parser and tests added. Xri parser added.

---
+++ library/Zend/OpenId/Discovery/Xrds/Parser/AbstractParser.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_OpenId
+ * @subpackage Zend_OpenId_Discovery
+ * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * @namespace
+ */
+namespace Zend\OpenId\Discovery\Xrds\Parser;
+use Zend\OpenId,
+    Zend\OpenId\Discovery\Xrds\Exception\ParseFailedException as ParseFailed;
+
+/**
+ * Default XRDS Parser Implementation
+ *
+ * @category   Zend
+ * @package    Zend_OpenId
+ * @subpackage Zend_OpenId_Discovery
+ * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+abstract class AbstractParser
+{
+    /**
+     * Parse input XRDS string into Descriptor object
+     *
+     * @param stirng $input Input XML 
+     *
+     * @return \Zend\OpenId\Discovery\Xrds\Element\Descriptor
+     */
+    public function parse($input)
+    {
+        $tree = $this->loadString($input);
+        $descriptor = $this->createDescriptor();
+
+        foreach($this->extractServices($tree) as $service) {
+            $descriptor->addService(
+                $this->createService($service)
+            );
+        }
+
+        return $descriptor;
+    }
+
+    /**
+     * Load XRDS string into tree
+     *
+     * @param stirng $input Input XML 
+     *
+     * @return \SimpleXMLElement
+     */
+    private function loadString($input)
+    {
+        libxml_use_internal_errors(true);
+        $tree = simplexml_load_string($input);
+        if (!$tree) {
+            $msg = "Failed loading XML\n";
+            foreach(libxml_get_errors() as $error) {
+                $msg .= "\t" . $error->message;
+            }
+            throw new ParseFailed($msg);
+        }
+        return $tree;
+    }
+
+}
---
+++ library/Zend/OpenId/Discovery/Xrds/Parser/Xri.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_OpenId
+ * @subpackage Zend_OpenId_Discovery
+ * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * @namespace
+ */
+namespace Zend\OpenId\Discovery\Xrds\Parser;
+use Zend\OpenId,
+    Zend\OpenId\Discovery\Xrds\Element\Descriptor\Xri as Descriptor,
+    Zend\OpenId\Discovery\Xrds\Element\ServiceEndpoint\Xri as ServiceEndpoint;
+
+/**
+ * XRI parser for XRDS documents
+ *
+ * @category   Zend
+ * @package    Zend_OpenId
+ * @subpackage Zend_OpenId_Discovery
+ * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Xri
+    extends OpenId\Discovery\Xrds\Parser\AbstractParser
+    implements OpenId\Discovery\Xrds\Parser
+{
+    /**
+     * Create XRID element
+     *
+     * @return \Zend\OpenId\Discovery\Xrds\Element\Descriptor
+     */
+    public function createDescriptor()
+    {
+        return new Descriptor();
+    }
+
+    /**
+     * Create XRD Service
+     *
+     * @return \Zend\OpenId\Discovery\Xrds\Element\Service
+     */
+    public function createService(\SimpleXMLElement $el)
+    {
+        $attr = $el->attributes();
+
+        $service = new ServiceEndpoint();
+
+        // type
+        if ($el->Type->count()) {
+            foreach ($el->Type as $type) {
+                $service->addType(trim((string)$type));
+            }
+        }
+
+        // service location
+        if ($el->URI->count()) {
+            foreach ($el->URI as $uri) {
+                $service->addUri(trim((string)$uri));
+            }
+        }
+
+        return $service;
+    }
+
+    /**
+     * Extract services from SimpleXMLElement
+     *
+     * return \SimpleXMLElement
+     */
+    public function extractServices(\SimpleXMLElement $el)
+    {
+        return $el->XRIDescriptor->Service;
+    }
+}
