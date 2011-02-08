f8cffaa14e56d91f041090ad23bfcfd6a65acbd5
Victor Farazdagi <simple.square@gmail.com>
2011-02-08 15:56:33 +0300

    Zend\OpenId: Updated specs in docblocks to XRI 2.0 CD2

--- library/Zend/OpenId/Discovery/Xrds/Element/ServiceEndpoint/Xri.php
+++ library/Zend/OpenId/Discovery/Xrds/Element/ServiceEndpoint/Xri.php
@@ -24,23 +24,32 @@
  */
 namespace Zend\OpenId\Discovery\Xrds\Element\ServiceEndpoint;
 use Zend\OpenId,
+    Zend\OpenId\Discovery\Xrds\Element,
     Zend\OpenId\Discovery\Xrds\Exception;
 
 /**
- * Container to encapsulate XRDS Service data for XRI 2.0 schema
+ * Container to encapsulate XRDS Service data for XRI Resolution 2.0 schema
  *
- * From XRI 2.0 Resolution Specs:
- * <xs:element name="Service"> 
- *  <xs:complexType>
- *      <xs:sequence>
- *          <xs:element ref="xrid:Type" minOccurs="0"/>
- *          <xs:group ref="xrid:URI" maxOccurs="unbounded"/>
- *          <xs:element ref="xrid:MediaType" minOccurs="0" maxOccurs="unbounded"/>
- *          <xs:group ref="xrid:otherelement" minOccurs="0" maxOccurs="unbounded"/>
- *      </xs:sequence>
- *      <xs:attributeGroup ref="xrid:otherattribute"/>
- *  </xs:complexType>
- * </xs:element>
+ * From XRI 2.0 Resolution Specs (Community Draft 2):
+ * <xs:element name="Service">
+ *      <xs:complexType>
+ *          <xs:sequence>
+ *              <xs:element ref="xrd:ProviderID" minOccurs="0"/>
+ *              <xs:element ref="xrd:Type" minOccurs="0" maxOccurs="unbounded"/>
+ *              <xs:element ref="xrd:Path" minOccurs="0" maxOccurs="unbounded"/>
+ *              <xs:element ref="xrd:MediaType" minOccurs="0" maxOccurs="unbounded"/>
+ *              <xs:choice>
+ *                  <xs:element ref="xrd:URI" minOccurs="0" maxOccurs="unbounded"/>
+ *                  <xs:element ref="xrd:Redirect" minOccurs="0" maxOccurs="unbounded"/>
+ *                  <xs:element ref="xrd:Ref" minOccurs="0" maxOccurs="unbounded"/>
+ *              </xs:choice>
+ *              <xs:element ref="xrd:LocalID" minOccurs="0" maxOccurs="unbounded"/>
+ *              <xs:group ref="xrd:otherelement" minOccurs="0" maxOccurs="unbounded"/>
+ *          </xs:sequence>
+ *          <xs:attributeGroup ref="xrd:priorityAttrGrp"/>
+ *          <xs:attributeGroup ref="xrd:otherattribute"/>
+ *      </xs:complexType>
+ *  </xs:element>
  *
  * @category   Zend
  * @package    Zend_OpenId
@@ -49,55 +58,6 @@ use Zend\OpenId,
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class Xri
-    extends    OpenId\Discovery\Xrds\Element\ServiceEndpoint\AbstractServiceEndpoint
-    implements OpenId\Discovery\Xrds\Element\ServiceEndpoint
-{
-    /**
-     * Add service type
-     * Acc. to schema XRI defines only 0 or 1 type element inside XRID's service
-     * element. Therefore method is overridden to preserve singularity of type.
-     *
-     * @param string $type Type of service being described
-     *
-     * @return \Zend\OpenId\Discovery\Xrds\Element\ServiceEndpoint
-     */
-    public function addType($type)
-    {
-        $this->reset(self::RESET_TYPES);
-        return parent::addType($type);
-    }
-
-    /**
-     * Fetch (by key) service type
-     * Acc. to schema XRI defines only 0 or 1 type element inside XRID's service
-     * element. Therefore method is overridden to preserve singularity of type.
-     *
-     * @param int $key Zero based index of element
-     *
-     * @return string
-     */
-    public function getType($key = 0)
-    {
-        if ($key !== 0) {
-            throw new Exception\ElementFailsValidationException(
-                'XRI 2.0 Resolution specified 0 or 1 modifier for ' .
-                'xrid:XRIDescriptor/xrid:Service/xrid:Type');
-        }
-        return parent::getType($key);
-    }
-
-    /**
-     * Set several types at once
-     * Acc. to schema XRI defines only 0 or 1 type element inside XRID's service
-     * element. Therefore method is overridden to preserve singularity of type.
-     *
-     * @param array $list Array of strings
-     *
-     * @return \Zend\OpenId\Discovery\Xrds\Element\ServiceEndpoint
-     */
-    public function setTypes($list)
-    {
-        // only last element is passed to parent
-        return parent::setTypes(array_slice($list, -1));
-    }
-}
+    extends    BaseServiceEndpoint
+    implements Element\ServiceEndpoint
+{}
