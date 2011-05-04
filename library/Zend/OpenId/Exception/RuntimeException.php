0976a9449ba2d8e7acf75527e1d3e3997511e79e
Victor Farazdagi <simple.square@gmail.com>
2011-05-04 14:52:59 +0400

    Added marker Exception interface and fixed copy year

--- library/Zend/OpenId/Exception/RuntimeException.php
+++ library/Zend/OpenId/Exception/RuntimeException.php
@@ -14,7 +14,7 @@
  *
  * @category   Zend
  * @package    Zend_OpenId
- * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
@@ -27,7 +27,7 @@ namespace Zend\OpenId\Exception;
  * @category   Zend
  * @package    Zend_OpenId
  * @uses       \Zend\OpenId\Exception
- * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class RuntimeException
